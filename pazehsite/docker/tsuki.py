14a4c33e2cd2b415f3f460c3bf17f3391cbf61e3
a8568730 <a8568730@hotmail.com>
2021-09-04 18:53:16 +0800

    * Nginx + Docker.

---
+++ pazehsite/docker/tsuki.py
@@ -0,0 +1,10 @@
+ALLOWED_HOSTS = [
+    # For deploy
+    'pazeh-kimgiok-2184.ithuan.tw',
+    # Enable docker BDD to link to docker django
+    'localhost',
+]
+
+DEBUG = False
+
+STATIC_ROOT = '/staticfiles/'
